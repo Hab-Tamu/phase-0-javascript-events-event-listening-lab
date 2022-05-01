a7884c05b9874e4397f14cf6e95bc709ddda0d8c
Habtamu Debele <habtlig2010@gmail.com>
2022-05-01 18:44:27 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+
+
+
 function addingEventListener() {
+    const input = document.getElementById('input')
+function clickAlert(){
+    alert("I was clicked!")
+    alert("Who am I")
 }
+
+input.addEventListener('click', clickAlert)
+}
+
+addingEventListener()
